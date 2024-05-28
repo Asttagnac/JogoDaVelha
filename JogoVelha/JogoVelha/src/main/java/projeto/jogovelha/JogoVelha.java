72b873e9d4e60dbc479066f6cd2d137094535f6d
João Victor <joaovictorvial11@gmail.com>
2024-05-28 18:38:29 -0300

    Add files via upload

---
+++ JogoVelha/JogoVelha/src/main/java/projeto/jogovelha/JogoVelha.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projeto.jogovelha;
+
+/**
+ *
+ * @author joao-
+ */
+public class JogoVelha {
+    
+    
+     public static void main(String[] args){
+         /* Chamando o Frame Jogo */
+         
+         Jogo FrameJogo=new Jogo();
+         FrameJogo.setVisible(true);
+     }
+}
